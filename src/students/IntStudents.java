159b15e729bfd8d858b48703562b309f2c34d79a
bensonlangli <bensonlangli@gmail.com>
2022-05-24 12:59:26 -0400

    initial commit

---
+++ src/students/IntStudents.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package students;
+
+/**
+ *
+ * @author benso
+ */
+public class IntStudents extends Students{
+    private String country;
+}
